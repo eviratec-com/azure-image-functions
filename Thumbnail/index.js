bcc018fdb2c7f7d75bd9c0fce1e8ac051f31d8d0
Callan Peter Milne <callanpmilne@gmail.com>
2023-04-09 22:23:48 +1000

    Updated Callbacks

--- Thumbnail/index.js
+++ Thumbnail/index.js
@@ -17,24 +17,24 @@ module.exports = async function (context, myQueueItem) {
       extension
     ] = blobItemUrl.match(/([0-9]{7,})\/([0-9]{4})\/([0-9]{1,2})\/([0-9]{1,2})\/(.+)\.([a-z]{3,})$/);
 
-    context.log(
-      `User: ${user} | Year: ${year} | Month: ${month} | Day: ${day} | `
-      + `Filename: ${filename} | Extension: ${extension} | BlobItemUrl: ${blobItemUrl}`
-    );
+    // context.log(
+    //   `User: ${user} | Year: ${year} | Month: ${month} | Day: ${day} | `
+    //   + `Filename: ${filename} | Extension: ${extension} | BlobItemUrl: ${blobItemUrl}`
+    // );
 
     try {
       const buff = await resizeImage(blobItemUrl, 200);
-      context.log('resizeImage result:');
-      context.log(buff);
-      context.log(containerName);
-      context.log(`${user}/${year}/${month}/${day}/${filename}.${extension}`);
+      // context.log('resizeImage result:');
+      // context.log(buff);
+      // context.log(containerName);
+      // context.log(`${user}/${year}/${month}/${day}/${filename}.${extension}`);
       const result = await streamOutputToFile(
         buff,
         containerName,
         `${user}/${year}/${month}/${day}/${filename}.${extension}`
       );
-      context.log('streamOutputToFileResult result:');
-      context.log(result);
+      // context.log('streamOutputToFileResult result:');
+      // context.log(result);
     }
     catch (e) {
       context.log(e.message);
